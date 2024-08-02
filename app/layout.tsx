322c63cd8f3db4fe26fd50c149eb0d282d43f31c
OdinBits <nishantchavan.dev@gmail.com>
2024-08-02 18:10:07 +0530

    Implemented project structure and files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import './globals.css'
+import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -9,21 +13,33 @@ const fontSans = FontSans({
 })
 
 export const metadata: Metadata = {
-  title: "Create Next App",
-  description: "Your go-to colaborator",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body  className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}>{children}</body>
-    </html>
-  );
+  title: 'LiveDocs',
+  description: 'Your go-to collaborative editor',
 }
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { 
+          colorPrimary: "#3371FF" ,
+          fontSize: '16px'
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Provider>
+            {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+} 
